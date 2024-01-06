d77f8bbb3e31832ad48293b6f31fa2c8baeeada3
Ryan Wern Jian Ng <145283919+ryanng02@users.noreply.github.com>
2024-01-06 14:53:01 +0800

    Add files via upload

---
+++ script-stt.js
@@ -0,0 +1,114 @@
+const paragraphs = [
+    "Their politician was, in this moment, a notour paperback. The first armless grouse is, in its own way, a gear. The coat is a wash. However, a cake is the llama of a caravan. Snakelike armies show us how playgrounds can be viscoses. Framed in a different way, they were lost without the fatal dogsled that composed their waitress. Far from the truth, the cockney freezer reveals itself as a wiggly tornado to those who look. The first hawklike sack.",
+    "Authors often misinterpret the lettuce as a folklore rabbi, when in actuality it feels more like an uncursed bacon. Pursued distances show us how mother-in-laws can be charleses. Authors often misinterpret the lion as a cormous science, when in actuality it feels more like a leprous lasagna. Recent controversy aside, their band was, in this moment, a racemed suit. The clutch of a joke becomes a togaed chair. The first pickled chess is.",
+    "In modern times the first scrawny kitten is, in its own way, an input. An ostrich is the beginner of a roast. An appressed exhaust is a gun of the mind. A recorder is a grade from the right perspective. A hygienic is the cowbell of a skin. Few can name a dun brazil that isn't a highbrow playroom. The unwished beast comes from a thorny oxygen. An insured advantage's respect comes with it the thought that the lucid specialist is a fix.",
+    "What we don't know for sure is whether or not a pig of the coast is assumed to be a hardback pilot. The literature would have us believe that a dusky clave is not but an objective. Few can name a limbate leo that isn't a sunlit silver. The bow is a mitten. However, the drawer is a bay. If this was somewhat unclear, few can name a paunchy blue that isn't a conoid bow. The undrunk railway reveals itself as a downstage bamboo to those who look.",
+    "An aunt is a bassoon from the right perspective. As far as we can estimate, some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals itself as a sclerous llama to those who look. A hip is the skirt of a peak. Some hempy laundries are thought of simply as orchids. A gum is a trumpet from the right perspective. A freebie flight is a wrench of the mind. Some posit the croupy.",
+    "A baby is a shingle from the right perspective. Before defenses, collars were only operations. Bails are gleesome relatives. An alloy is a streetcar's debt. A fighter of the scarecrow is assumed to be a leisured laundry. A stamp can hardly be considered a peddling payment without also being a crocodile. A skill is a meteorology's fan. Their scent was, in this moment, a hidden feeling. The competitor of a bacon becomes a boxlike cougar.",
+    "A broadband jam is a network of the mind. One cannot separate chickens from glowing periods. A production is a faucet from the right perspective. The lines could be said to resemble zincoid females. A deborah is a tractor's whale. Cod are elite japans. Some posit the wiglike norwegian to be less than plashy. A pennoned windchime's burst comes with it the thought that the printed trombone is a supply. Relations are restless tests.",
+    "In recent years, some teeming herons are thought of simply as numbers. Nowhere is it disputed that an unlaid fur is a marble of the mind. Far from the truth, few can name a glossy lier that isn't an ingrate bone. The chicken is a giraffe. They were lost without the abscessed leek that composed their fowl. An interviewer is a tussal bomb. Vanward maracas show us how scarfs can be doubts. Few can name an unguled punch that isn't pig.",
+    "A cough is a talk from the right perspective. A designed tractor's tray comes with it the thought that the snuffly flax is a rainbow. Their health was, in this moment, an earthy passbook. This could be, or perhaps the swordfishes could be said to resemble healthy sessions. A capricorn is a helium from the right perspective. However, a sled is a mailman's tennis. The competitor of an alarm becomes a toeless raincoat. Their twist was, in this moment.",
+    "Authors often misinterpret the flag as a wayless trigonometry, when in actuality it feels more like a bousy gold. Few can name a jasp oven that isn't a stutter grape. They were lost without the huffy religion that composed their booklet. Those waves are nothing more than pedestrians. Few can name a quartered semicolon that isn't a rounding scooter. Though we assume the latter, the literature would have us believe.",
+    "This could be, or perhaps few can name a pasteboard quiver that isn't a brittle alligator. A swordfish is a death's numeric. Authors often misinterpret the mist as a swelling asphalt, when in actuality it feels more like a crosswise closet. Some posit the tonal brother-in-law to be less than newborn. We know that the sizes could be said to resemble sleepwalk cycles. Before seasons, supplies were only fighters. Their stew was, in this moment.",
+    "The vision of an attempt becomes a lawny output. Dibbles are mis womens. The olden penalty reveals itself as a bustled field to those who look. Few can name a chalky force that isn't a primate literature. However, they were lost without the gamy screen that composed their beret. Nowhere is it disputed that a step-uncle is a factory from the right perspective. One cannot separate paints from dreary windows. What we don't know for sure is whether.",
+    "A tramp is a siamese from the right perspective. We know that a flitting monkey's jaw comes with it the thought that the submersed break is a pamphlet. Their cream was, in this moment, a seedy daffodil. The nest is a visitor. Far from the truth, they were lost without the released linen that composed their step-sister. A vibraphone can hardly be considered a pardine process without also being an archaeology. The bay of a hyacinth becomes.",
+    "The frosts could be said to resemble backstage chards. One cannot separate colleges from pinkish bacons. Far from the truth, the mom of a rooster becomes a chordal hydrogen. A tempo can hardly be considered a purer credit without also being a pajama. The first combined ease is, in its own way, a pantyhose. Extending this logic, the guides could be said to resemble reddest monkeies. Framed in a different way, an addle hemp is a van.",
+    "Far from the truth, an ajar reminder without catamarans is truly a foundation of smarmy semicircles. An alike board without harps is truly a satin of fated pans. A hubcap sees a parent as a painful beautician. The zeitgeist contends that some intense twigs are thought of simply as effects. A cross is a poppied tune. The valanced list reveals itself as an exchanged wrist to those who look. Recent controversy aside.",
+    "The hefty opinion reveals itself as a sterile peer-to-peer to those who look. This could be, or perhaps the watch of a diamond becomes a bosom baboon. In recent years, some posit the unstuffed road to be less than altern. It's an undeniable fact, really; the livelong lettuce reveals itself as an unstuffed soda to those who look. In ancient times a bit is a balance's season. The popcorn of a morning becomes a moonless beauty.",
+    "If this was somewhat unclear, a friend is a fridge from the right perspective. An upset carriage is a stitch of the mind. To be more specific, a temper is a pair from the right perspective. Authors often misinterpret the liquid as a notchy baseball, when in actuality it feels more like an unbarbed angle. Though we assume the latter, the first vagrom report is, in its own way, a tower. We know that the octopus of a cd becomes an unrent dahlia.",
+    "A reptant discussion's rest comes with it the thought that the condemned syrup is a wish. The drake of a wallaby becomes a sonant harp. If this was somewhat unclear, spotty children show us how technicians can be jumps. Their honey was, in this moment, an intime direction. A ship is the lion of a hate. They were lost without the croupous jeep that composed their lily. In modern times a butcher of the birth is assumed to be a spiral bean.",
+    "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless toy. A board is the cast of a religion. In ancient times the first stinko sailboat is, in its own way, an exchange. Few can name a tutti channel that isn't a footless operation. Extending this logic, an oatmeal is the rooster of a shake. Those step-sons are nothing more than matches.",
+]; //this is an array called paragraphs
+
+const typingText = document.querySelector(".typing-text p") //making fixed variables from index
+const inpField = document.querySelector(".wrapper .input-field")
+const tryAgainBtn = document.getElementById("try-again-btn");//getting icon using ID 
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector(".wpm span")
+const cpmTag = document.querySelector(".cpm span")
+
+let timer; //making not fixed variables
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length); //variable that generates rando index within 19 as math.random only generates >=0 and <=1
+    typingText.innerHTML = ""; //clearing the thing making sure nothing is there before we begin
+    paragraphs[ranIndex].split("").forEach(char => { //.split is to split the string into an array after we get the string from a randomly selected paragraph; the " " is to split each individual character into its own element in the array
+        console.log(char); //for each is looping thru all elements in the array; console log is just to log each element in the console though it is not necesarry rather just for debugging
+        let span = `<span>${char}</span>` //${char} is a placeholder that gets replaced with the actual value of the variable; basically wrapping each character in its own span; this type of looping for each chracter is for better individual control over each character later on
+        typingText.innerHTML += span; //appending back each character so that the front end will see a whole paragraph
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active"); //this is to make the first character a CSS class ="active"; .add("active") is a method of the classList property; adding active is needed to visually highlight the current character one should be typing
+    document.addEventListener("keydown", () => inpField.focus()); //this adds an eventlistener to the entire document; so when a key is pressed it called back the inpField.focus function which sets focus on the input field; This ensures that when any key is pressed anywhere on the document, the focus is immediately set to the input field.
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span"); //this is all characters included
+    let typedChar = inpField.value.split("")[charIndex]; //encapsulate the current typed character into its own element; doing all this can be called through the typedChar variable
+    if (charIndex < characters.length - 1 && timeLeft > 0) { //checking if we are done with the words and if there is still time left
+        if (!isTyping) { //checking if the var isTyping is false which is 0
+            timer = setInterval(initTimer, 1000); //setInterval is a built-in function used for repeatedly executing a function or piece of code at specified intervals; In this case, setInterval calls the function initTimer every 1000 miliseconds which is 1 second and assigns the returned timer ID to the var 'Timer';this is also to start the timer if it has not started
+            isTyping = true; //this indicates that timer has started and prevents prevents timer from being set up again 
+        }
+        if (typedChar == null) { //checks if user has typed nothing; true when it is nothing
+            if (charIndex > 0) {
+                charIndex--; //decrements the counter when user is backspacing
+                if (characters[charIndex].classList.contains("incorrect")) { //checks if the previously backspaced character had been deemed incorrect
+                    mistakes--; //if it has, we now remove one
+                }
+                characters[charIndex].classList.remove("correct", "incorrect"); //removes any previous class that being incorrect or correct on the element to reset the styling of the character pointed to by the charIndex
+            }
+        } else { //if user actually typed a something
+            if (characters[charIndex].innerText == typedChar) { //checks if it matches
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active")); //iterates through the characters array and removes CSS class "active"; reseting the styling on all characters
+        characters[charIndex].classList.add("active"); //this is to visually highlight current character 
+
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60); // (charIndex - mistakes) this is net characters correctly typed; we devide this by an assumed 5 words to estimate number of words typed based on the characters; (maxTime - timeLeft) is the time used; ((charIndex - mistakes) / 5) / (maxTime - timeLeft) this is in words per second typed so to make it per minute we times 60
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0: wpm; //wpm<0 checks if it is below 0; it also checks if wpm is false meaning null or 0; Checks if the value of wpm is equal to positive infinity. This condition is true when the typing speed is calculated as infinity, which may happen if no time has passed; the ? is basically saying if any condition mentioned previously are true then wpm is set to 0, if not then remain unchanged
+
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer); //clearing timer interval so that it can stop updating the ID
+        inpField.value = ""; //clear input field and also prevents any more input from user after time is out
+    }
+}
+
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
